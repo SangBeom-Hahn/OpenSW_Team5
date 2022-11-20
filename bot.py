204c971df4d6b767283ed5ab14bd4d53534c3c64
SangBeom-Hahn <hsb990917@gmail.com>
2022-11-20 22:33:48 +0900

    bot message complete

--- bot.py
+++ bot.py
@@ -3,13 +3,17 @@
 import telegram
 import schedule
 import time
+import datetime
+import pytz
 
 token = "5759317578:AAHB10nZxKvOzsBJBw8aBlWamsMWC7A1Fnk"
 bot = telegram.Bot(token)
 public_chat_name = "@ktest2022"
 
 def job():
-    now = time .localtime()
+    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    if now.hour >= 23 or now.hour <= 6:
+        return
     text = f"current time = {0}".format(str(now))
     bot.sendMessage(chat_id = public_chat_name, text = "alarm").chat_id
     # print("current time = ", str(now))
@@ -17,28 +21,7 @@ def job():
 schedule.every(1).minutes.do(job)
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(30)
 
 # # id_channel = bot.sendMessage(chat_id = public_chat_name, text = "alarm").chat_id
 # print(id_channel)
-
-
-
-
-import schedule
-import time
-import telegram
-token = "5759317578:AAHB10nZxKvOzsBJBw8aBlWamsMWC7A1Fnk"
-bot = telegram.Bot(token = token)
-chat_id = "5776108466"
-
-def job():
-    now = time .localtime()
-    text = f"current time = {0}".format(str(now))
-    bot.sendMessage(chat_id=chat_id, text=text)
-    # print("current time = ", str(now))
-
-schedule.every(1).minutes.do(job)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
