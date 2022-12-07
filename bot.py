2463d579b97f57c49a715eee37beb65b8fedeeb5
hcu55 <hcu55@naver.com>
2022-12-07 17:45:35 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 # - 채널 만들기
 import telegram
 import schedule
@@ -6,22 +5,26 @@ import time
 import datetime
 import pytz
 
-token = "여기 토큰인데 깃헙에서 토큰 가리라고 메일옴;;"
+token = ""
 bot = telegram.Bot(token)
 public_chat_name = "@ktest2022"
+
 
 def job():
     now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
     if now.hour >= 23 or now.hour <= 6:
         return
-    text = f"current time = {0}".format(str(now))
-    bot.sendMessage(chat_id = public_chat_name, text = "alarm").chat_id
-    # print("current time = ", str(now))
+    
+    else :
+        text=('alarm : '+str(now))
+        bot.sendMessage(chat_id = public_chat_name, text = text).chat_id
+        #print("current time = ", str(now))
 
-schedule.every(1).minutes.do(job)
+schedule.every(30).minutes.do(job)
 while True:
     schedule.run_pending()
     time.sleep(30)
+    
 
 # # id_channel = bot.sendMessage(chat_id = public_chat_name, text = "alarm").chat_id
 # print(id_channel)
